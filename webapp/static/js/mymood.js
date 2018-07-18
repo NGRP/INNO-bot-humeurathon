64343921c2a8db917a7d71f0479b4c66716cc7c8
To-Thi Hoang <to-thi.hoang@viseo.com>
2018-07-18 23:39:28 +0200

    add confirm

--- webapp/static/js/mymood.js
+++ webapp/static/js/mymood.js
@@ -1,8 +1,27 @@
 $(window).load(function() {
+	$('.alert-danger').hide();
 	$('.container.comment').hide();
 	$('.choose-image').click(function() {
 		$('#photoInput').val($(this).data('picture'));
 		$(".container.mood").hide();
 		$(".container.comment").show();
 	});
+
+	$('#commentForm').submit(function(event) {
+		event.preventDefault();
+
+		$.post($(this).attr('action'), $(this).serialize())
+		.success(function(data) {
+			if(data.redirect) {
+				location.href = data.redirect;
+			}
+		}).fail(function(xhr, textStatus, errorThrown) {
+            $('.alert-danger').hide();
+            if(xhr.status === 403) {
+                $('#forbiddenAlert').show();
+            } else if(xhr.status === 400) {
+            	$('#unknownError').show()
+            }
+        })
+	})
 })
