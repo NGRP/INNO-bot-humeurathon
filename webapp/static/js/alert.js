8d7be2eaedff0386b32534c1f4cbfd26bc9cb740
To-Thi Hoang <to-thi.hoang@viseo.com>
2018-07-19 16:40:34 +0200

    front report alert

---
+++ webapp/static/js/alert.js
@@ -0,0 +1,48 @@
+$(window).load(function() {
+	$.datepicker.setDefaults( $.datepicker.regional[ "fr" ] );
+
+	$('.collapse').each(function() {
+		$(this).on('show.bs.collapse', function() {
+			$('.collapse:not(#'+$(this).attr('id')+')').collapse('hide');
+		});
+
+		$(this).find('.list-group-item-action').click(function() {
+			var input = $(this).data('value-for');
+			$(input).val($(this).data('value'));
+
+			$(input+'Span').html($(this).html());
+
+			$('.collapse').collapse('hide');
+		})
+	});
+	$('#dateInput').datepicker({
+
+		onSelect: function(text, datepicker) {
+			var date = new Date(datepicker.selectedYear, datepicker.selectedMonth, datepicker.selectedDay);
+			$("#dateInputSpan").html($.datepicker.formatDate( "d MM yy", date ));
+			$('#dateInputHidden').val(date.toISOString());
+		}
+	});
+	$("#dateInputSpan").click(function() {
+		$('#dateInput').show().focus().hide();
+	})
+
+
+	$('#alertForm').submit(function(event) {
+		event.preventDefault();
+
+		$.post($(this).attr('action'), $(this).serialize())
+		.success(function(data) {
+			if(data.redirect) {
+				location.href = data.redirect;
+			}
+		}).fail(function(xhr, textStatus, errorThrown) {
+            $('.alert-danger').hide();
+            if(xhr.status === 403) {
+                $('#forbiddenAlert').show();
+            } else if(xhr.status === 400) {
+            	$('#unknownError').show()
+            }
+        })
+	})
+});
