80572d474ca52a81392f7905e961070e2fec29d5
To-Thi Hoang <to-thi.hoang@viseo.com>
2018-07-19 17:41:25 +0200

    start login

--- webapp/static/js/me.js
+++ webapp/static/js/me.js
@@ -1,11 +1,16 @@
 $(window).load(function() {
 	var url = location.href,
-		modal = url.substring(url.indexOf('#'));
+		hash = url.indexOf('#'),
+		modal = url.substring(hash);
 
-	if(modal.length > 1) {
+	if(hash !== -1 && modal.length > 1) {
 		$(modal).modal();
 		$(modal).on('hide.bs.modal', function() {
 			history.pushState("", document.title, window.location.pathname + window.location.search);
 		})
+	}
+
+	if($('#welcome').length === 1) {
+		$('#welcome').modal({ backdrop: 'static', keyboard: false });
 	}
 });
